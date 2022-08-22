14c821beb8d1bbcd1f929a9d5d1f3c78d4c88880
frail <ashutosh.routray126@gmail.com>
2022-08-22 09:14:02 +0530

    need to finish grid paths

---
+++ Introductory Problems/GridPaths.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+
+using namespace std;
+
+ll board[7][7];
+
+int main()
+{
+    for(ll i=0;i<7;i++)
+    {
+        for(ll j=0;j<7;j++)
+        {
+            board[i][j]=0;
+        }
+    }
+    solve()
+    return 0;
+}
+
+//needs to be finished
